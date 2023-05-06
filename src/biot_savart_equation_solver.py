b34bc300949112f452d9ae5021c8a71d0c53d933
Mégane Bergeron <megane.bergeron.1@ulaval.ca>
2023-05-06 16:16:34 -0400

    bio savart

--- src/biot_savart_equation_solver.py
+++ src/biot_savart_equation_solver.py
@@ -1,7 +1,7 @@
 import numpy as np
 from scipy.constants import mu_0, pi
 
-from src.coordinate_and_position import CoordinateSystem
+;from src.coordinate_and_position import CoordinateSystem
 from src.fields import VectorField
 
 
@@ -38,7 +38,21 @@ class BiotSavartEquationSolver:
             B_z(x, y) are the 3 components of the magnetic vector at a given point (x, y) in space. Note that
             B_x = B_y = 0 is always True in our 2D world.
         """
-        raise NotImplementedError
+        position, current = [], []
+        for row, row_value in enumerate(electric_current):
+            for column, cell in enumerate(row_value):
+                if cell[0] or cell[1] != 0:
+                    position.append((row, column, 0))
+                    current.append(cell)
+        magnetic_field = np.zeros(np.shape(electric_current))
+        for row, row_value in enumerate(magnetic_field):
+            for column, cell in enumerate(row_value):
+                if (row, column, 0) not in position:
+                    r = np.array([row, column, 0]) - np.array(position)
+                    r_norm = (np.linalg.norm(r, axis=1))**3
+                    cross_product = np.cross(current, r)
+                    magnetic_field[row, column] = [int(0),int(0),np.sum(mu_0 * cross_product[:,2] / (4 * pi * r_norm))]
+        return VectorField(magnetic_field)
 
     def _solve_in_polar_coordinate(
             self,
