4673d6ff23a0495e7cede0a6ab20069666094cde
Mahelie486 <117679771+Mahelie486@users.noreply.github.com>
2023-05-07 12:10:54 -0400

    Update __init__.py

--- src/__init__.py
+++ src/__init__.py
@@ -1,6 +1,6 @@
 from src.circuit import Circuit
 from src.coordinate_and_position import CoordinateSystem
-from src.electrical_components import Wire, VoltageSource
+from src.electrical_components import VoltageSource
 from src.world import World
 
-__all__ = ["Circuit", "CoordinateSystem", "VoltageSource", "Wire", "World"]
+__all__ = ["Circuit", "CoordinateSystem", "VoltageSource", "World"]
