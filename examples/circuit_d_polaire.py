87ed42137c8b179f5c878d4004d486330f91bc18
Mégane Bergeron <megane.bergeron.1@ulaval.ca>
2023-05-11 13:46:02 -0400

    d polaire

--- examples/circuit_d_polaire.py
+++ examples/circuit_d_polaire.py
@@ -1,51 +1,58 @@
-import env_examples
-
-#from src import Circuit, Current, Wire, World
-#import time
-#!!en polaire aussi celui-ci!!
-#polaire + cartésien
 import env_examples  # Modifies path, DO NOT REMOVE
 
 from sympy import Symbol
 
-from math import cos, sin, pi, sqrt, asin
-
 from src import Circuit, CoordinateSystem, VoltageSource, Wire, World
 
+import numpy as np
 
-# Essai paramètres polaire pour cercle
 if __name__ == "__main__":
-    WORLD_SHAPE = (100, 2*pi)
+    WORLD_SHAPE = (101, 101)
     BATTERY_VOLTAGE = 1.0
     HIGH_WIRE_RESISTANCE = 1.0
     LOW_WIRE_RESISTANCE = 0.01
 
-    polar_variables = Symbol("r"), Symbol("theta")
-    r, theta = polar_variables
+    cartesian_variables = Symbol("x"), Symbol("y")
+    x, y = cartesian_variables
 
-    # Eqn pour dpl rayon
-    expression_dpl_rayon = r
-    expression_dpl_theta = theta*0
-    rayon_eqn = (expression_dpl_rayon, expression_dpl_theta)
+    x_expression_vertical = 0 * x
+    y_expression_vertical = y
+    vertical_eqs = (x_expression_vertical, y_expression_vertical)
 
-    # Eqn dpl circum
-    expression_dpl_rayon = 0 * r
-    expression_dpl_theta = theta
-    theta_eqn = (expression_dpl_rayon, expression_dpl_theta)
+    x_expression_horizontal = x
+    y_expression_horizontal = 0 * y
+    horizontal_eqs = (x_expression_horizontal, y_expression_horizontal)
 
-# on trace juste un cercle pour les tests
+    x_expression_diagonal = x
+    y_expression_diagonal = y
+    diagonal_eqs = (x_expression_diagonal, y_expression_diagonal)
+
+    theta_1 = np.pi / 24
+    theta_2 = np.pi / 3
+
     wires = [
-        Wire((2, pi/2), (2, pi), theta_eqn, polar_variables, LOW_WIRE_RESISTANCE),
-        Wire((2, pi), (1, pi), rayon_eqn, polar_variables, HIGH_WIRE_RESISTANCE),
-        Wire((1, pi), (1, pi/2), theta_eqn, polar_variables, LOW_WIRE_RESISTANCE),
-        VoltageSource((1, pi/2), (2, pi/2), rayon_eqn, polar_variables, BATTERY_VOLTAGE)
+        Wire((60, theta_1), (74, theta_1), horizontal_eqs, cartesian_variables, LOW_WIRE_RESISTANCE),
+        Wire((74, theta_1), (74, np.pi / 7), vertical_eqs, cartesian_variables, LOW_WIRE_RESISTANCE),
+        Wire((74, np.pi / 7), (74, 2*np.pi / 9), vertical_eqs, cartesian_variables, HIGH_WIRE_RESISTANCE),
+        Wire((74, 2*np.pi / 9), (74, theta_2), vertical_eqs, cartesian_variables, LOW_WIRE_RESISTANCE),
+        Wire((74, theta_2), (60, theta_2), horizontal_eqs, cartesian_variables, LOW_WIRE_RESISTANCE),
+        Wire((60, theta_2), (60, 2*np.pi / 9), vertical_eqs, cartesian_variables, LOW_WIRE_RESISTANCE),
+        VoltageSource((60, 2*np.pi / 9), (60, np.pi / 7), vertical_eqs, cartesian_variables, BATTERY_VOLTAGE),
+        Wire((60, np.pi / 7), (60, theta_1), vertical_eqs, cartesian_variables, LOW_WIRE_RESISTANCE),
     ]
-    ground_position = (2, pi/2)
+    ground_position = (60, 2*np.pi / 9)
 
     circuit = Circuit(wires, ground_position)
-    world = World(circuit=circuit, coordinate_system=CoordinateSystem.POLAR, shape=WORLD_SHAPE)
+    world = World(circuit=circuit, coordinate_system=CoordinateSystem.CARTESIAN, shape=WORLD_SHAPE)
     world.show_circuit(
-        {0: (2, pi/2), 1: (2, pi), 2: (1, pi), 3: (1, pi/2)}
+        {0: (60, theta_1),
+        1: (74, theta_1),
+        2: (74, np.pi / 7),
+        3: (74, 2*np.pi / 9), 
+        4: (74, theta_2), 
+        5: (60, theta_2), 
+        6: (60, 2*np.pi / 9),
+        7: (60, np.pi / 7)}
     )
     world.compute()
     world.show_all()
