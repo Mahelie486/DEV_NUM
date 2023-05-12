7961f04e5c41fd076453632ea9661ab2e7393dfe
Mégane Bergeron <megane.bergeron.1@ulaval.ca>
2023-05-12 14:18:15 -0400

    d polaire

--- examples/circuit_d_polaire.py
+++ examples/circuit_d_polaire.py
@@ -32,27 +32,27 @@ if __name__ == "__main__":
 
     wires = [
         Wire((60, theta_1), (74, theta_1), horizontal_eqs, cartesian_variables, LOW_WIRE_RESISTANCE),
-        Wire((74, theta_1), (74, np.pi / 7), vertical_eqs, cartesian_variables, LOW_WIRE_RESISTANCE),
-        Wire((74, np.pi / 7), (74, 2*np.pi / 9), vertical_eqs, cartesian_variables, HIGH_WIRE_RESISTANCE),
-        Wire((74, 2*np.pi / 9), (74, theta_2), vertical_eqs, cartesian_variables, LOW_WIRE_RESISTANCE),
+        Wire((74, theta_1), (74, np.pi/7), vertical_eqs, cartesian_variables, LOW_WIRE_RESISTANCE),
+        Wire((74, np.pi/7), (74, 2*np.pi/9), vertical_eqs, cartesian_variables, HIGH_WIRE_RESISTANCE),
+        Wire((74, 2*np.pi/9), (74, theta_2), vertical_eqs, cartesian_variables, LOW_WIRE_RESISTANCE),
         Wire((74, theta_2), (60, theta_2), horizontal_eqs, cartesian_variables, LOW_WIRE_RESISTANCE),
-        Wire((60, theta_2), (60, 2*np.pi / 9), vertical_eqs, cartesian_variables, LOW_WIRE_RESISTANCE),
-        VoltageSource((60, 2*np.pi / 9), (60, np.pi / 7), vertical_eqs, cartesian_variables, BATTERY_VOLTAGE),
-        Wire((60, np.pi / 7), (60, theta_1), vertical_eqs, cartesian_variables, LOW_WIRE_RESISTANCE),
+        Wire((60, theta_2), (60, 2*np.pi/9), vertical_eqs, cartesian_variables, LOW_WIRE_RESISTANCE),
+        VoltageSource((60, 2*np.pi/9), (60, np.pi/7), vertical_eqs, cartesian_variables, BATTERY_VOLTAGE),
+        Wire((60, np.pi/7), (60, theta_1), vertical_eqs, cartesian_variables, LOW_WIRE_RESISTANCE),
     ]
-    ground_position = (60, np.pi / 7)
+    ground_position = (60, np.pi/7)
 
     circuit = Circuit(wires, ground_position)
     world = World(circuit=circuit, coordinate_system=CoordinateSystem.CARTESIAN, shape=WORLD_SHAPE)
     world.show_circuit(
         {0: (60, theta_1),
         1: (74, theta_1),
-        2: (74, np.pi / 7),
-        3: (74, 2*np.pi / 9), 
+        2: (74, np.pi/7),
+        3: (74, 2*np.pi/9), 
         4: (74, theta_2), 
         5: (60, theta_2), 
-        6: (60, 2*np.pi / 9),
-        7: (60, np.pi / 7)}
+        6: (60, 2*np.pi/9),
+        7: (60, np.pi/7)}
     )
     world.compute()
     world.show_all()
