34941bd3cddf6409bd270c19ad8075c1f3068446
Mahelie486 <117679771+Mahelie486@users.noreply.github.com>
2023-05-13 17:08:04 -0400

    Update circuit_d_polaire.py

--- examples/circuit_d_polaire.py
+++ examples/circuit_d_polaire.py
@@ -48,7 +48,7 @@ if __name__ == "__main__":
 
     circuit = Circuit(wires, ground_position)
     world = World(circuit=circuit, coordinate_system=CoordinateSystem.CARTESIAN, shape=WORLD_SHAPE)
-    """
+    
     world.show_circuit(
         {0: (60, theta_1),
         1: (74, theta_1),
@@ -61,16 +61,16 @@ if __name__ == "__main__":
     )
     world.compute()
     world.show_all()
-    """
+    
     a, b = circuit.get_voltage_and_current_fields(WORLD_SHAPE, [60,60], [101, 101])
     # print(a)  # a est le voltage en tout point = aussi un scalar
-    """
-    laplace = LaplaceEquationSolver()
-    tests =  laplace._solve_in_polar_coordinate(a, 1, 1)  # Permet de checker ce qui est retourné par Laplace pour ce circuit
-    print(tests)
-    """
-    Biot = BiotSavartEquationSolver()
+    
+    #laplace = LaplaceEquationSolver()
+    #tests =  laplace._solve_in_polar_coordinate(a, 1, 1)  # Permet de checker ce qui est retourné par Laplace pour ce circuit
+    #print(tests)
+    
+    #Biot = BiotSavartEquationSolver()
     # print(len(b[0]))
     # print(b)
-    Magn =  Biot._solve_in_polar_coordinate(b, 1, 1)  # Permet de checker ce qui est retourné par Laplace pour ce circuit
-    print(Magn)
+    #Magn =  Biot._solve_in_polar_coordinate(b, 1, 1)  # Permet de checker ce qui est retourné par Laplace pour ce circuit
+    #print(Magn)
