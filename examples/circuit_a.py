5d1b4338dc2e350cb80d7b25fb6d6e0241f0cf56
Mégane Bergeron <megane.bergeron.1@ulaval.ca>
2023-05-13 16:39:42 -0400

    circuit a

--- examples/circuit_a.py
+++ examples/circuit_a.py
@@ -26,16 +26,16 @@ if __name__ == "__main__":
     horizontal_eqs = (x_expression_horizontal, y_expression_horizontal)
 
     wires = [
-        Wire((0, 0), (0, 5), vertical_eqs, cartesian_variables, LOW_WIRE_RESISTANCE),
-        Wire((0, 5), (3, 5), horizontal_eqs, cartesian_variables, LOW_WIRE_RESISTANCE),
-        Wire((3, 5), (4, 5), horizontal_eqs, cartesian_variables, HIGH_WIRE_RESISTANCE),
-        Wire((4, 5), (5, 5), horizontal_eqs, cartesian_variables, LOW_WIRE_RESISTANCE),
-        Wire((5, 5), (5, 2), vertical_eqs, cartesian_variables, LOW_WIRE_RESISTANCE),
-        Wire((5, 2), (5, 1), vertical_eqs, cartesian_variables, HIGH_WIRE_RESISTANCE),
-        Wire((5, 1), (5, 0), vertical_eqs, cartesian_variables, LOW_WIRE_RESISTANCE),
-        Wire((5, 0), (2, 0), horizontal_eqs, cartesian_variables, LOW_WIRE_RESISTANCE),
-        VoltageSource((2, 0), (1, 0), horizontal_eqs, cartesian_variables, BATTERY_VOLTAGE),
-        Wire((1, 0), (0, 0), horizontal_eqs, cartesian_variables, LOW_WIRE_RESISTANCE)
+        Wire((0, 0), (0, 100), vertical_eqs, cartesian_variables, LOW_WIRE_RESISTANCE),
+        Wire((0, 100), (60, 100), horizontal_eqs, cartesian_variables, LOW_WIRE_RESISTANCE),
+        Wire((60, 100), (80, 100), horizontal_eqs, cartesian_variables, HIGH_WIRE_RESISTANCE),
+        Wire((80, 100), (100, 100), horizontal_eqs, cartesian_variables, LOW_WIRE_RESISTANCE),
+        Wire((100, 100), (100, 40), vertical_eqs, cartesian_variables, LOW_WIRE_RESISTANCE),
+        Wire((100, 40), (100, 20), vertical_eqs, cartesian_variables, HIGH_WIRE_RESISTANCE),
+        Wire((100, 20), (100, 0), vertical_eqs, cartesian_variables, LOW_WIRE_RESISTANCE),
+        Wire((100, 0), (40, 0), horizontal_eqs, cartesian_variables, LOW_WIRE_RESISTANCE),
+        VoltageSource((40, 0), (20, 0), horizontal_eqs, cartesian_variables, BATTERY_VOLTAGE),
+        Wire((20, 0), (0, 0), horizontal_eqs, cartesian_variables, LOW_WIRE_RESISTANCE)
     ]
     ground_position = (1, 0)
 
@@ -43,12 +43,12 @@ if __name__ == "__main__":
     world = World(circuit=circuit, coordinate_system=CoordinateSystem.CARTESIAN, shape=WORLD_SHAPE)
     """
     world.show_circuit(
-        {0: (0, 0), 1: (0, 5), 2: (3, 5), 3: (4, 5), 4: (5, 5), 5: (5, 2), 6: (5, 1), 7: (5, 0), 8: (2, 0), 9: (1, 0)}
+        {0: (0, 0), 1: (0, 100), 2: (60, 100), 3: (80, 100), 4: (100, 100), 5: (100, 40), 6: (100, 20), 7: (100, 0), 8: (40, 0), 9: (20, 0)}
     )
     world.compute()
     world.show_all()
     """
-    a, b = circuit.get_voltage_and_current_fields(WORLD_SHAPE, [0,0], [6, 6])
+    a, b = circuit.get_voltage_and_current_fields(WORLD_SHAPE, [0,0], [6, 6]) #quand c'ai fais X20 je l'ai pas fais ici
     # print(a)  # a est le voltage en tout point = aussi un scalar
     """
     laplace = LaplaceEquationSolver()
