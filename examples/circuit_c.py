19e2ffca0baa99dabfd2e54f93e07a3798800575
Mahelie486 <117679771+Mahelie486@users.noreply.github.com>
2023-05-14 15:01:28 -0400

    Update circuit_c.py

--- examples/circuit_c.py
+++ examples/circuit_c.py
@@ -52,10 +52,3 @@ if __name__ == "__main__":
     )
     world.compute()
     world.show_all()
-    
-
-    # a, b = circuit.get_voltage_and_current_fields(WORLD_SHAPE, [60,60], [101, 101])
-    # print(a)  # a est le voltage en tout point = aussi un scalar
-    #laplace = LaplaceEquationSolver()
-    #tests =  laplace._solve_in_cartesian_coordinate(a, 1, 1)  # Permet de checker ce qui est retourné par Laplace pour ce circuit
-    #print(tests)
