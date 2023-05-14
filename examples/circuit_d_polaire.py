b35574d3eb7d38e06d5bc678917e5247b586a249
Mahelie486 <117679771+Mahelie486@users.noreply.github.com>
2023-05-14 11:50:18 -0400

    Update circuit_d_polaire.py

--- examples/circuit_d_polaire.py
+++ examples/circuit_d_polaire.py
@@ -16,48 +16,48 @@ if __name__ == "__main__":
     HIGH_WIRE_RESISTANCE = 1.0
     LOW_WIRE_RESISTANCE = 0.01
 
-    cartesian_variables = Symbol("x"), Symbol("y")
-    x, y = cartesian_variables
+    polar_variables = Symbol("r"), Symbol("theta")
+    r, theta = polar_variables
 
-    x_expression_vertical = 0 * x
-    y_expression_vertical = y
-    vertical_eqs = (x_expression_vertical, y_expression_vertical)
+    r_expression_vertical = 0 * r
+    theta_expression_vertical = theta
+    vertical_eqs = (r_expression_vertical, theta_expression_vertical)
 
-    x_expression_horizontal = x
-    y_expression_horizontal = 0 * y
-    horizontal_eqs = (x_expression_horizontal, y_expression_horizontal)
+    r_expression_horizontal = r
+    theta_expression_horizontal = 0 * theta
+    horizontal_eqs = (r_expression_horizontal, theta_expression_horizontal)
 
-    x_expression_diagonal = x
-    y_expression_diagonal = y
-    diagonal_eqs = (x_expression_diagonal, y_expression_diagonal)
+    r_expression_diagonal = r
+    theta_expression_diagonal = theta
+    diagonal_eqs = (r_expression_diagonal, theta_expression_diagonal)
 
     theta_1 = np.pi / 24
     theta_2 = np.pi / 3
 
     wires = [
-        Wire((60, theta_1), (74, theta_1), horizontal_eqs, cartesian_variables, LOW_WIRE_RESISTANCE),
-        Wire((74, theta_1), (74, np.pi/7), vertical_eqs, cartesian_variables, LOW_WIRE_RESISTANCE),
-        Wire((74, np.pi/7), (74, 2*np.pi/9), vertical_eqs, cartesian_variables, HIGH_WIRE_RESISTANCE),
-        Wire((74, 2*np.pi/9), (74, theta_2), vertical_eqs, cartesian_variables, LOW_WIRE_RESISTANCE),
-        Wire((74, theta_2), (60, theta_2), horizontal_eqs, cartesian_variables, LOW_WIRE_RESISTANCE),
-        Wire((60, theta_2), (60, 2*np.pi/9), vertical_eqs, cartesian_variables, LOW_WIRE_RESISTANCE),
-        VoltageSource((60, 2*np.pi/9), (60, np.pi/7), vertical_eqs, cartesian_variables, BATTERY_VOLTAGE),
-        Wire((60, np.pi/7), (60, theta_1), vertical_eqs, cartesian_variables, LOW_WIRE_RESISTANCE),
+        Wire((20, theta_1), (80, theta_1), horizontal_eqs, polar_variables, LOW_WIRE_RESISTANCE),
+        Wire((80, theta_1), (80, np.pi/7), vertical_eqs, polar_variables, LOW_WIRE_RESISTANCE),
+        Wire((80, np.pi/7), (80, 2*np.pi/9), vertical_eqs, polar_variables, HIGH_WIRE_RESISTANCE),
+        Wire((80, 2*np.pi/9), (80, theta_2), vertical_eqs, polar_variables, LOW_WIRE_RESISTANCE),
+        Wire((80, theta_2), (20, theta_2), horizontal_eqs, polar_variables, LOW_WIRE_RESISTANCE),
+        Wire((20, theta_2), (20, 2*np.pi/9), vertical_eqs, polar_variables, LOW_WIRE_RESISTANCE),
+        VoltageSource((20, 2*np.pi/9), (20, np.pi/7), vertical_eqs, polar_variables, BATTERY_VOLTAGE),
+        Wire((20, np.pi/7), (20, theta_1), vertical_eqs, polar_variables, LOW_WIRE_RESISTANCE),
     ]
-    ground_position = (60, np.pi/7)
+    ground_position = (20, np.pi/7)
 
     circuit = Circuit(wires, ground_position)
     world = World(circuit=circuit, coordinate_system=CoordinateSystem.POLAR, shape=WORLD_SHAPE)
     
     world.show_circuit(
-        {0: (60, theta_1),
-        1: (74, theta_1),
-        2: (74, np.pi/7),
-        3: (74, 2*np.pi/9), 
-        4: (74, theta_2), 
-        5: (60, theta_2), 
-        6: (60, 2*np.pi/9),
-        7: (60, np.pi/7)}
+        {0: (20, theta_1),
+        1: (80, theta_1),
+        2: (80, np.pi/7),
+        3: (80, 2*np.pi/9), 
+        4: (80, theta_2), 
+        5: (20, theta_2), 
+        6: (20, 2*np.pi/9),
+        7: (20, np.pi/7)}
     )
     world.compute()
     world.show_all()
